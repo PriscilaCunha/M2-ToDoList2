ec5f9f81159174639d701fef7ac29e91d71d5bfc
PriscilaCunha <PriscilaCunha@msn.com>
2023-05-21 12:47:19 -0300

    Aula 2

---
+++ src/screens/TodoForm/TodoForm.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const TodoForm = () => {
+    return (
+        <View>
+            <Text>Todo Form</Text>
+        </View>
+    )
+}
+
+export default TodoForm;
---
+++ src/screens/TodoList/TodoList.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const TodoList = () => {
+    return (
+        <View>
+            <Text>Todo List</Text>
+        </View>
+    )
+}
+
+export default TodoList;
